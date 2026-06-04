d4caafb7dcae88941a4d2411146205e0a17d7d97
Max Litvin <letuwwa@gmail.com>
2026-06-04 10:51:30 +0300

    feat: db models and alembic

---
+++ app/db/models/user.py
@@ -0,0 +1,32 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.models.base_model import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+
+    username: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False,
+    )
+
+    surname: Mapped[str] = mapped_column(
+        String(30),
+        nullable=False,
+    )
+    last_name: Mapped[str] = mapped_column(
+        String(30),
+        nullable=False,
+    )
+
+    is_active: Mapped[bool] = mapped_column(default=True)
---
+++ app/db/session.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "postgresql://admin:admin@localhost:5433/mydbf"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
