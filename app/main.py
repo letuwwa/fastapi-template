f418fe71e0427978cd63f8b4b4d6bab32c63681c
Max Litvin <letuwwa@gmail.com>
2026-06-21 11:03:21 +0300

    fix: cors regex update

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.allowed_origins,
+    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1):\d+$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
